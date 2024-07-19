2626dc4358c9b4cc848cad340c8414ceebd11dfc
cherrycoder9 <cherrycoder9@gmail.com>
2024-07-19 16:11:17 +0900

    Initial commit

---
+++ src/test/java/hrdsystem/HrDsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hrdsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrDsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
